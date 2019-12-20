05d452b260bb4fdb0836348a3ebe370a7a0679a2
Ted <ski2per@163.com>
2019-12-20 16:19:08 +0800

    add validation for login form

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,5 +1,7 @@
 <template>
-  <v-form @submit.prevent="login">
+  <v-form 
+    ref="loginForm"
+    >
     <v-text-field
       v-model="username"
       :rules="[rules.required]"
@@ -18,7 +20,7 @@
       @click:append="show = !show"
     ></v-text-field>
 
-    <v-btn color="warning" type="submit">Login</v-btn>
+    <v-btn color="warning" @click="validate" :disabled="!valid">Login</v-btn>
   </v-form>
 </template>
 
@@ -26,6 +28,7 @@
   export default {
     data(){
       return {
+        valid : true,
         username: "",
         password : "",
         show: false,
@@ -37,6 +40,13 @@
       }
     },
     methods: {
+      validate () {
+        this.valid = false
+        if(this.$refs.loginForm.validate()) {
+          this.login()
+        }
+        this.valid = true
+      }, 
       login: function () {
         let username = this.username
         let password = this.password
@@ -44,13 +54,6 @@
         const params = new FormData()
         params.append('username', username)
         params.append('password', password)
-
-        //debug
-        console.log('[LoginForm.vue]')
-        for (var key of params.entries()) {
-			    console.log(key[0] + ', ' + key[1])
-		    }
-        //debug
 
         this.$store.dispatch('login', params)
        .then(() => this.$router.push('/user'))
