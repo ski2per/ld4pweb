e22bbef1726eca31dff10e7cf9d3beabd0cf97ac
Ted <ski2per@163.com>
2019-12-25 17:35:50 +0800

    working on AboutMe page

---
+++ src/components/PasswordUpdate.vue
@@ -0,0 +1,70 @@
+<template>
+  <v-form 
+    ref="passwordForm"
+    @submit.prevent="validate"
+    >
+    <v-text-field
+      v-model="oldPassword"
+      :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
+      :rules="[rules.required, rules.min]"
+      :type="show ? 'text' : 'password'"
+      name="input-password"
+      label="旧密码"
+      @click:append="show = !show"
+      color="green"
+    ></v-text-field>
+
+    <v-text-field
+      v-model="newPassword"
+      :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
+      :rules="[rules.required, rules.min]"
+      :type="show ? 'text' : 'password'"
+      name="input-password"
+      label="新密码"
+      @click:append="show = !show"
+      color="green"
+    ></v-text-field>
+
+    <v-text-field
+      v-model="newPasswordAgain"
+      :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
+      :rules="[rules.required, rules.min]"
+      :type="show ? 'text' : 'password'"
+      name="input-password"
+      label="再来一次"
+      @click:append="show = !show"
+      color="green"
+    ></v-text-field>
+
+    <v-btn color="warning" type="submit" :disabled="!valid">修改密码</v-btn>
+  </v-form>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        valid: true,
+        show: false,
+        oldPassword : "",
+        newPassword : "",
+        newPasswordAgain : "",
+        username: "",
+        rules: {
+          required: value => !!value || 'Required.',
+          min: v => v.length >= 1 || 'Min 1 characters',
+          emailMatch: () => ('The email and password you entered don\'t match'),
+        }
+      }
+    }, //data()
+    methods: {
+      validate () {
+        this.valid = false
+        if(this.$refs.passwordForm.validate()) {
+          console.log("Form ok, gonna change psd")
+        }
+        this.valid = true
+      },
+    },
+  } //export default
+</script>
--- src/views/AboutMe.vue
+++ src/views/AboutMe.vue
@@ -1,11 +1,17 @@
 <template>
   <v-container justify="start">
   about me
+    <password-update></password-update>
   </v-container>
 </template>
 
 <script>
+import PasswordUpdate from '@/components/PasswordUpdate.vue'
+
 export default {
   name: 'About',
+  components: {
+    'password-update': PasswordUpdate
+  }
 }
 </script>
